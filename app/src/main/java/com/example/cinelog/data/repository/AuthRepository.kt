649bbf6e9ffb6439483a6ce820275362ebeff44b
Vane2005 <vanessaduranmona@gmail.com>
2026-04-20 12:20:33 -0500

    feat: send implementation of user registration

--- app/src/main/java/com/example/cinelog/data/repository/AuthRepository.kt
+++ app/src/main/java/com/example/cinelog/data/repository/AuthRepository.kt
@@ -1,11 +1,40 @@
 package com.example.cinelog.data.repository
 
+import com.example.cinelog.domain.model.User
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.tasks.await
+
 class AuthRepository {
-    fun login(username: String, password: String): Result<Boolean> {
+
+    private val auth = FirebaseAuth.getInstance()
+    private val firestore = FirebaseFirestore.getInstance()
+
+    suspend fun login(username: String, password: String): Result<Boolean> {
         return if (username.isNotEmpty() && password.isNotEmpty()) {
             Result.success(true)
         } else {
             Result.failure(Exception("Credenciales inválidas"))
         }
     }
+
+    suspend fun register(nombre: String, edad: Int, email: String, password: String): Result<Boolean> {
+        return try {
+
+            // Crear usuario en Authentication
+            val authResult = auth.createUserWithEmailAndPassword(email, password).await()
+            val uid = authResult.user?.uid ?: return Result.failure(Exception("Error al obtener el UID"))
+
+            // Guardar datos en Firestore
+            val user = User(uid = uid, nombre = nombre, edad = edad, email = email)
+            firestore.collection("users").document(uid).set(user).await()
+
+            Result.success(true)
+
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
+
+
 }
